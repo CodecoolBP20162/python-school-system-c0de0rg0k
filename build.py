e86e33153ccb7945a5ad82742298e03e4980d0b5
VargaDavid <varga.david.gamf@gmail.com>
2017-01-25 14:57:07 +0100

    Build.py for build datatables

--- build.py
+++ build.py
@@ -2,6 +2,27 @@
 
 from models import *
 
-db.connect()
-# List the tables here what you want to create...
-db.create_tables([], safe=True)
+try:
+    db.connect()
+except:
+    print('I can\'t connect to the database')
+
+if School.table_exists():
+    School.drop_table()
+
+if Applicant.table_exists():
+    Applicant.drop_table()
+
+if City.table_exists():
+    City.drop_table()
+
+if Mentor.table_exists():
+    Mentor.drop_table()
+
+if InterviewSlot.table_exists():
+    InterviewSlot.drop_table()
+
+if Interview.table_exists():
+    Interview.drop_table()
+
+db.create_tables([School, Applicant, City, Mentor, InterviewSlot, Interview], safe=True)
