046821f69e28c2f9455a9833b47eabc629998947
VargaDavid <varga.david.gamf@gmail.com>
2017-01-26 13:44:12 +0100

    Main.py with basic functions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from applicants_status import applicants_status
 
 
 def clear_sreen():
@@ -31,15 +32,15 @@ def main():
                 chosen_administrator_menu = int(input("Please choose an Administrator menu number: "))
 
                 if chosen_administrator_menu == 1:
-                    # call mentor menu
+                    # call administrator menu
                     pass
 
                 elif chosen_administrator_menu == 2:
-                    # call mentor menu
+                    # call administrator menu
                     pass
 
                 elif chosen_administrator_menu == 3:
-                    # call applicant menu
+                    # call administrator menu
                     pass
 
                 elif chosen_administrator_menu == 0:
@@ -87,26 +88,27 @@ def main():
             chosen_applicant_menu = 'q'
             while chosen_applicant_menu != 0:
                 print("\n- - - School system - Applicant Menu - - -")
-                print("1. ")
-                print("2. ")
-                print("3. ")
+                print("1. Application details")
+                print("2. School details")
+                print("3. Questions")
                 print("0. Exit")
                 print("-------------------------------------")
                 chosen_applicant_menu = int(input("Please choose an Applicant menu number: "))
 
-                if chosen_menu == 1:
-                    # call mentor menu
-                    pass
+                if chosen_applicant_menu == 1:
+                    app_code = input("Please tell me your application code: ")
+                    status = applicants_status(app_code)
+                    print("Your application status is ", status)
 
-                elif chosen_menu == 2:
-                    # call mentor menu
-                    pass
-
-                elif chosen_menu == 3:
+                elif chosen_applicant_menu == 2:
                     # call applicant menu
                     pass
 
-                elif chosen_menu == 0:
+                elif chosen_applicant_menu == 3:
+                    # call applicant menu
+                    pass
+
+                elif chosen_applicant_menu == 0:
                     clear_sreen()
                     break
 
