c18c27d07a6bfe81eaf9d1f43d720c45ceabfd02
VargaDavid <varga.david.gamf@gmail.com>
2017-01-27 10:33:51 +0100

    Generate interview date to applicants menu implemented

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from build import BuildTable
 from example_data import GenerateExampleData
 from new_applicants import GenerateApplicants
 from applicants_school import applicants_school
+from sort_applicants import sort_applicants
+from closest_interview import ApplicantGenerator
 
 
 def clear_sreen():
@@ -27,10 +29,12 @@ def main():
             clear_sreen()
             chosen_administrator_menu = 'q'
             while chosen_administrator_menu != 0:
-                print("\n- - - School system - Administrator Menu - - -")
+                print("\n- - - School system - Administrator Menu - - -\n-------------------------------------")
                 print("1. Create tables")
                 print("2. Generate data")
                 print("3. Generate applicants")
+                print("4. Generate interview date to applicants")
+                print("5. List all applicants")
                 print("0. Exit")
                 print("-------------------------------------")
                 chosen_administrator_menu = int(input("Please choose an Administrator menu number: "))
@@ -56,6 +60,45 @@ def main():
                     except:
                         print("I can't Generate applicants")
 
+                elif chosen_administrator_menu == 4:
+                    try:
+                        ApplicantGenerator()
+                        print("Interview dates successfully generated to applicants")
+                    except:
+                        print("Something went wrong. I can't generate interview dates to applicants")
+
+                elif chosen_administrator_menu == 5:
+                    clear_sreen()
+                    print("\n- - - School system - Administrator Menu - Sort applicants - - -\n-------------------------------------")
+                    print("1. By Status")
+                    print("2. By Time")
+                    print("3. By Location")
+                    print("4. By Personal data (name, email)")
+                    print("5. By School")
+                    print("6. By Mentor name")
+                    print("0. Exit")
+                    print("-------------------------------------")
+                    chosen_sort_menu = input("Choose a menu number to sort applicants: ")
+                    if chosen_sort_menu == 1:
+                        ordered_applicants = sort_applicants("status")
+                    elif chosen_sort_menu == 2:
+                        pass
+                    elif chosen_sort_menu == 3:
+                        ordered_applicants = sort_applicants()
+                        print(ordered_applicants)
+                        for applicant in ordered_applicants:
+                            print(applicant.id, applicant.first_name, applicant.last_name, applicant.applicant_city /
+                                  applicant.applicant_code, applicant.status)
+                    elif chosen_sort_menu == 4:
+                        pass
+                    elif chosen_sort_menu == 5:
+                        pass
+                    elif chosen_sort_menu == 6:
+                        pass
+                    elif chosen_sort_menu == 0:
+                        clear_sreen()
+                        break
+
                 elif chosen_administrator_menu == 0:
                     clear_sreen()
                     break
@@ -69,7 +112,7 @@ def main():
             clear_sreen()
             chosen_mentor_menu = 'q'
             while chosen_mentor_menu != 0:
-                print("\n- - - School system - Mentor Menu - - -")
+                print("\n- - - School system - Mentor Menu - - -\n-------------------------------------")
                 print("1. ")
                 print("2. ")
                 print("3. ")
@@ -100,7 +143,7 @@ def main():
             clear_sreen()
             chosen_applicant_menu = 'q'
             while chosen_applicant_menu != 0:
-                print("\n- - - School system - Applicant Menu - - -")
+                print("\n- - - School system - Applicant Menu - - -\n-------------------------------------")
                 print("1. Application details")
                 print("2. School details")
                 print("3. Questions")
