ec0d51f845f28aefd9e98c329beda3e6088380b2
keligeri <kel.gergo@gmail.com>
2017-02-08 20:10:54 +0100

    Can handle the dbname too

--- set_connection.py
+++ set_connection.py
@@ -2,6 +2,17 @@
 class SetConnection:
 
     def __init__(self, file_name='connect.txt'):
+        self.file_name = file_name
         self.username = ""
-        with open(file_name) as f:
-            self.username = f.read()
+        self.dbname = ""
+
+        self.__read_files()
+
+    def __read_files(self):
+        with open(self.file_name) as f:
+            line = f.readline().split(",")
+            if len(line) == 1:
+                self.username = line[0]
+            else:
+                self.dbname = line[0]
+                self.username = line[1]
