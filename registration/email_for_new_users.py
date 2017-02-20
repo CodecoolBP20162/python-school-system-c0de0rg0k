b00344bd47fd9662952f8fceb096b941bc36551f
VargaDavid <varga.david.gamf@gmail.com>
2017-02-20 14:54:47 +0100

    Email sending added for new mentors

--- registration/email_for_new_users.py
+++ registration/email_for_new_users.py
@@ -10,15 +10,31 @@ class SendEmail:
         self.password = 'Codeorgok123'
 
     # Writing the message (this message will appear in the email)
-    def __message_text(self, applicant):
+    def __applicant_message_text(self, new_applicant):
         msg = "Hello {0} {1}!\nYour registration was successful at some site.\nYour school is {2}." \
               "\nFrom now you can log in using the following applicant code: {3}\nThank you for choosing us you fool!"\
-            .format(applicant.first_name, applicant.last_name, applicant.applied_school.city, applicant.applicant_code)
+            .format(new_applicant.first_name, new_applicant.last_name, new_applicant.applied_school.city,
+                    new_applicant.applicant_code)
+        return msg
+
+    def __mentor_message_text(self, new_mentor):
+        msg = "Hello {0} {1}!\nYour registration was successful at some site.\nYour school is {2}." \
+              "\nFrom now you can log in using the following mentor id: {3}\nThank you for choosing us you fool!"\
+            .format(new_mentor.first_name, new_mentor.last_name, new_mentor.school.city,
+                    new_mentor.id)
         return msg
 
     # Sending the mail
-    def send_email(self, applicant):
-        msg = self.__message_text(applicant)
+    def send_applicant_email(self, new_applicant):
+        msg = self.__applicant_message_text(new_applicant)
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.starttls()
+        server.login(self.username, self.password)
+        server.sendmail(self.fromaddr, self.toaddrs, msg)
+        server.quit()
+
+    def send_mentor_email(self, new_mentor):
+        msg = self.__mentor_message_text(new_mentor)
         server = smtplib.SMTP('smtp.gmail.com:587')
         server.starttls()
         server.login(self.username, self.password)
