202576666f93f9d0f3030ee9c3b6a1a8ff2fd44c
keligeri <kel.gergo@gmail.com>
2017-02-21 11:04:07 +0100

    Add a html and an app.py with a new route

--- app.py
+++ app.py
@@ -7,16 +7,6 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 
 app = Flask(__name__)  # create the application instance :)
 app.config.from_object(__name__)  # load config from this file , flaskr.py
-app.debug = True
-
-# Load default config and override config from an environment variable
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'School_system.db'),
-    SECRET_KEY='development key',
-    USERNAME='postgres',
-    PASSWORD='asdfg'
-))
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
 def init_db():
@@ -28,15 +18,18 @@ def index():
     return render_template('index_temp.html')
 
 
-
 @app.route('/admin/list_applicants')
 def list_applicants():
     applicants = Applicant.select().order_by(Applicant.id)
     return render_template('applicants.html', applicants=applicants)
 
 
+@app.route("/admin/e-mail-log", methods=["GET"])
+def show_sent_email():
+    applicant = Applicant.select()
+    render_template('show_email.html', header="List of all emails", object=applicant)
 
 
 if __name__ == '__main__':
     init_db()
-    app.run()
+    app.run(debug=True)
