647f6fcc5b534efa69e8f9320a6d992419664881
VargaDavid <varga.david.gamf@gmail.com>
2017-02-21 14:40:35 +0100

    Registration page added

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from peewee import *
 from models import *
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
+from generator.app_code_generator import AppCodeGenerator
+from generator.applicant_generator import ApplicantGenerator
 
 
 app = Flask(__name__)  # create the application instance :)
@@ -30,6 +32,21 @@ def list_applicants():
                            schools=schools)
 
 
+@app.route('/registration', methods=['GET'])
+def show_registration_form():
+    return render_template('applicant_registration.html')
+
+
+@app.route('/registration', methods=['POST'])
+def applicant_registration():
+    Applicant.create(first_name=request.form['first_name'],
+                     last_name=request.form['last_name'],
+                     applicant_city=request.form['applicant_city'],
+                     status="new",
+                     applied_school = ApplicantGenerator().search_nearest_school(request.form['applicant_city']),
+                     applicant_code= AppCodeGenerator().code_generator(),
+                     email=request.form['email_address'])
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
