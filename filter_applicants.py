c7cd1204d5d2b76af88e65ca67c84c3913ce636f
TahinDani <tahin.daniel@gmail.com>
2017-02-23 13:57:23 +0100

    staticmethod instead of classmethod, input check in app.py

--- filter_applicants.py
+++ filter_applicants.py
@@ -5,34 +5,31 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 
 class Filter_applicants:
 
-    @classmethod
-    def filter_applicants_name(cls):
+    @staticmethod
+    def filter_applicants_name():
         name_result = []
-        # subquery = Applicant.select()
-        if request.form['filter_input_name']:
-            for i in request.form['filter_input_name'].split():
-                i.lower()
-                try:
-                    foo = Applicant.select().where((fn.lower(Applicant.first_name) == i) | (fn.lower(Applicant.last_name) == i))#.get()
-                    for i in foo:
-                        if i not in name_result:
-                            name_result.append(i)
-                except:
-                    continue
-            return name_result  # list_applicants(applicants=cls.name_result)
-        else:
-            return None
+        for i in request.form['filter_input_name'].split():
+            i.lower()
+            try:
+                foo = Applicant.select().where((fn.lower(Applicant.first_name) == i) | (fn.lower(Applicant.last_name) == i))#.get()
+                for i in foo:
+                    if i not in name_result:
+                        name_result.append(i)
+            except:
+                continue
+        return name_result
 
-    @classmethod
-    def filter_applicants_email(cls):
+
+    @staticmethod
+    def filter_applicants_email():
         input = request.form['filter_input_email']
         bar = lambda x: x.email == input
         query = Applicant.select().order_by(Applicant.first_name)
         email_result = filter(bar, query)
         return email_result
 
-    @classmethod
-    def filter_applicants_city(cls):
+    @staticmethod
+    def filter_applicants_city():
         input = request.form['filter_input_city']
         bar = lambda x: x.applicant_city == input
         query = Applicant.select().order_by(Applicant.first_name)
@@ -40,8 +37,8 @@ class Filter_applicants:
         return city_result
 
 
-    @classmethod
-    def filter_applicants_school(cls):
+    @staticmethod
+    def filter_applicants_school():
         input = request.form['filter_input_school']
         bar = lambda x: x.applied_school.city == input
         query = Applicant.select().join(School).order_by(Applicant.first_name)
@@ -49,8 +46,8 @@ class Filter_applicants:
         return city_result
 
 
-    @classmethod
-    def filter_applicants_status(cls):
+    @staticmethod
+    def filter_applicants_status():
         input = request.form['filter_input_status']
         bar = lambda x: x.status == input
         query = Applicant.select().order_by(Applicant.first_name)
