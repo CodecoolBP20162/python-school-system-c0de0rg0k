ebb553fa052c1f9dca48c5695e4f32a9065de7af
VargaDavid <varga.david.gamf@gmail.com>
2017-03-07 13:49:26 +0100

    Already logged in possibilities handled

--- app.py
+++ app.py
@@ -33,19 +33,28 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    print(app.config['USERNAME'])
-    print(app.config['PASSWORD'])
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['admin_logged_in'] = True
-            return redirect(url_for('show_admin_menu'))
+    if session.get('applicant_logged_in'):
+        error = "You are already logged in as applicant. Please log out first"
+        return render_template('already_logged_in.html', error=error)
+    elif session.get('mentor_logged_in'):
+        error = "You are already logged in as mentor. Please log out first"
+        return render_template('already_logged_in.html', error=error)
+    elif session.get('admin_logged_in'):
+        return redirect(url_for('show_admin_menu'))
+    else:
+        print(app.config['USERNAME'])
+        print(app.config['PASSWORD'])
+        error = None
+        if request.method == 'POST':
+            if request.form['username'] != app.config['USERNAME']:
+                error = 'Invalid username'
+            elif request.form['password'] != app.config['PASSWORD']:
+                error = 'Invalid password'
+            else:
+                session['admin_logged_in'] = True
+                return redirect(url_for('show_admin_menu'))
 
-    return render_template('login.html', error=error)
+        return render_template('login.html', error=error)
 
 
 @app.route('/admin-login/', methods=["GET"])
@@ -80,7 +89,6 @@ def list_applicants(applicants=None):
                            cities=cities,
                            schools=schools,
                            statuses=statuses)
-
 
 
 @app.route('/admin/list_interviews')
@@ -195,11 +203,17 @@ def filter():
 
 @app.route("/applicant/login/", methods=["GET"])
 def applicant_login():
-    if not session.get('applicant_logged_in'):
+    if session.get('admin_logged_in'):
+        error = "You are already logged in as admin. Please log out first"
+        return render_template('already_logged_in.html', error=error)
+    elif session.get('mentor_logged_in'):
+        error = "You are already logged in as mentor. Please log out first"
+        return render_template('already_logged_in.html', error=error)
+    elif session.get('applicant_logged_in'):
+        return render_template('applicant_interface.html')
+    else:
         error = ""
         return render_template('applicant_login.html', error=error)
-    else:
-        return render_template('applicant_interface.html')
 
 
 @app.route("/applicant/login/", methods=["POST"])
@@ -223,11 +237,17 @@ def applicant_logout():
 
 @app.route("/mentor/login/", methods=["GET"])
 def mentor_login():
-    if not session.get('mentor_logged_in'):
+    if session.get('admin_logged_in'):
+        error = "You are already logged in as admin. Please log out first"
+        return render_template('already_logged_in.html', error=error)
+    elif session.get('applicant_logged_in'):
+        error = "You are already logged in as applicant. Please log out first"
+        return render_template('already_logged_in.html', error=error)
+    elif session.get('mentor_logged_in'):
+        return render_template('mentor_interface.html')
+    else:
         error = ""
         return render_template('mentor_login.html', error=error)
-    else:
-        return render_template('mentor_interface.html')
 
 
 @app.route("/mentor/login/", methods=["POST"])
@@ -247,6 +267,7 @@ def validate_mentor():
 def mentor_logout():
     session.pop('mentor_logged_in', None)
     return redirect(url_for('mentor_login'))
+
 
 
 if __name__ == '__main__':
