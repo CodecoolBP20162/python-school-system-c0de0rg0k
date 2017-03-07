21ededfbc429437c4d21b1aa556ccd449e4ae406
keligeri <kel.gergo@gmail.com>
2017-03-07 15:06:14 +0100

    About us at withput mentors

--- app.py
+++ app.py
@@ -31,13 +31,19 @@ def index():
     return render_template('homepage_home.html')
 
 
-@app.route('/about_the_training')
+@app.route('/about-the-training')
 def about_training():
     return render_template("homepage_about_training.html")
+
 
 @app.route('/principles')
 def principles():
     return render_template("homepage_principles.html")
+
+
+@app.route('/about-us')
+def about_us():
+    return render_template("homepage_about_us.html")
 
 
 @app.route('/login', methods=['GET', 'POST'])
