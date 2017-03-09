e6c4cc44b9a6ba22d972853ac733cc3863bbae90
TahinDani <tahin.daniel@gmail.com>
2017-03-09 17:17:45 +0100

    logout button repositioned

--- app.py
+++ app.py
@@ -333,7 +333,7 @@ def logout():
       
 @app.route('/contact')
 def contact():
-    return render_template('contact.html', page="contact")
+    return render_template('contact.html', page="cont")
 
 
 if __name__ == '__main__':
